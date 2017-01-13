58c0453f5d5fbc5f97e3dff0435288bc6f807500
kelleyv <kelleyv@CHPFME011X.emea.hubersuhner.net>
2017-01-13 11:25:58 +0100

    Test

--- src/TestCases/AllTests.java
+++ src/TestCases/AllTests.java
@@ -5,7 +5,12 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ ColourTests.class, PenTests.class })
+@SuiteClasses(
+		{ 
+			ColourTests.class, 
+			PenTests.class 
+		}
+		)
 public class AllTests {
 
 }
---
+++ src/TestCases/Test.java
@@ -0,0 +1,15 @@
+package TestCases;
+
+import static org.junit.Assert.*;
+
+import Paint.Brush;
+
+public class Test {
+
+	@org.junit.Test
+	public void testBrushGetColour() {
+		Brush b1 = new Brush(1,254,255, 81);
+		assertEquals(989, b1.getColour().getGreen());
+	}
+
+}
