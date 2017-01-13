0a7bc6de46f5f59d35e222d16c09c9cba53558dc
kelleryvo <keller.yvo@gmail.com>
2017-01-13 11:51:12 +0100

    Update Test.java

--- src/TestCases/Test.java
+++ src/TestCases/Test.java
@@ -9,6 +9,7 @@ public class Test {
 	@org.junit.Test
 	public void testBrushGetColour() {
 		Brush b1 = new Brush(1,254,255, 81);
+		brush b233sa = new Brush(41,84,521,81);
 		assertEquals(989, b1.getColour().getGreen());
 	}
 
